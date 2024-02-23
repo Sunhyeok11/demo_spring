17e853299bc789f12c39788626422609adf0d99d
SunHyeok <cghhld@naver.com>
2024-02-23 22:51:11 +0900

    생성자 주입

--- src/main/java/hello/demo/AppConfig.java
+++ src/main/java/hello/demo/AppConfig.java
@@ -7,6 +7,7 @@ import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRepository;
 import hello.demo.order.OrderService;
+import hello.demo.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,8 +30,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-        //return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
--- src/main/java/hello/demo/order/OrderServiceImpl.java
+++ src/main/java/hello/demo/order/OrderServiceImpl.java
@@ -3,14 +3,26 @@ package hello.demo.order;
 import hello.demo.discount.DiscountPolicy;
 import hello.demo.member.Member;
 import hello.demo.member.MemberRepository;
+import hello.demo.member.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements  OrderService{
 
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
